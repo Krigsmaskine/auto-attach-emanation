134dc0c074ad21adb00b3090c15452553ad1cf95
Krigsmaskine <39592771+Krigsmaskine@users.noreply.github.com>
2026-08-24 18:33:28 +0200

    Add isOwner safeguard to region update
    Always remember to test with multiple users

--- auto-attach-emanation/scripts/main.js
+++ auto-attach-emanation/scripts/main.js
@@ -19,40 +19,43 @@ Hooks.once("ready", () => {
     game.socket.on('module.auto-attach-emanation', (message) => {
         warnTheGM(message);
     }
-)});
+    )
+});
 
 Hooks.on("createRegion", async (...args) => {
     if (args[0].flags.dnd5e.origin) {
-        const originActivity = await fromUuid(args[0].flags.dnd5e.origin);
-        
-        if (originActivity.target.template.labels.type == "Emanation") {
-            let tokenDocument;
+        if (args[0].isOwner) {
+            const originActivity = await fromUuid(args[0].flags.dnd5e.origin);
 
-            // Only non-linked token documents include a "direct line" to the token document in the flags :(
-            if (originActivity.uuid.includes("Token")) {
-                // This could be done smarter, but we're finding the fourth period to pilfer the token document UUID from the longer activity UUID, so we know which to token document to attach to.
-                const fourthPeriod = nthIndex(originActivity.uuid, ".", 4);
-                tokenDocument = await fromUuid(originActivity.uuid.substring(0, fourthPeriod))
-                await args[0].update({ "attachment.token": tokenDocument });
+            if (originActivity.target.template.labels.type == "Emanation") {
+                let tokenDocument;
+
+                // Only non-linked token documents include a "direct line" to the token document in the flags :(
+                if (originActivity.uuid.includes("Token")) {
+                    // This could be done smarter, but we're finding the fourth period to pilfer the token document UUID from the longer activity UUID, so we know which to token document to attach to.
+                    const fourthPeriod = nthIndex(originActivity.uuid, ".", 4);
+                    tokenDocument = await fromUuid(originActivity.uuid.substring(0, fourthPeriod))
+                    await args[0].update({ "attachment.token": tokenDocument });
+
+                }
+
+                // Assume there's only 1 token of the linked character on the parent scene
+                else {
+                    const foundLinkedTokens = args[0].parent.tokens.filter((possibleToken) => possibleToken.actorId == originActivity.actor.id)
+                    if (foundLinkedTokens.length == 0) {
+                        game.socket.emit('module.auto-attach-emanation', `No tokens related to actor ${originActivity.actor.name} found on the scene, cannot automatically attach emanation region.`)
+                        return;
+                    }
+                    if (foundLinkedTokens.length > 1) {
+                        game.socket.emit('module.auto-attach-emanation', `Found more than 1 token related to actor ${originActivity.actor.name} on the scene, automatically attaching to one at random.`)
+                    }
+
+                    tokenDocument = args[0].parent.tokens.find((possibleToken) => possibleToken.actorId == originActivity.actor.id);
+                    await args[0].update({ "attachment.token": tokenDocument });
+                };
+
 
             }
-
-            // Assume there's only 1 token of the linked character on the parent scene
-            else {
-                const foundLinkedTokens = args[0].parent.tokens.filter((possibleToken) => possibleToken.actorId == originActivity.actor.id)
-                if (foundLinkedTokens.length == 0) {
-                    game.socket.emit('module.auto-attach-emanation', `No tokens related to actor ${originActivity.actor.name} found on the scene, cannot automatically attach emanation region.`)
-                    return;
-                }
-                if (foundLinkedTokens.length > 1) {
-                    game.socket.emit('module.auto-attach-emanation', `Found more than 1 token related to actor ${originActivity.actor.name} on the scene, automatically attaching to one at random.`)
-                }
-
-                tokenDocument = args[0].parent.tokens.find((possibleToken) => possibleToken.actorId == originActivity.actor.id);
-                await args[0].update({ "attachment.token": tokenDocument });
-            };
-
-
         }
-    }
+    };
 });
